7f6f186057517e7125283d52f40f67d728794532
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-10-04 20:47:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+object=[];
 value = "";
 function preload(){}
 
@@ -24,4 +25,48 @@ function modelLoaded()
 function draw()
 {
     image(video, 0 , 0 , 300, 300);
+    if(status != "")
+    {
+        objectdetection.detect(video, gotresults);
+        for(i=0; i<object.length; i++)
+        {
+            document.getElementById("statuss").innerHTML = "Objects Detected"
+            percent = floor(object[i].confidence * 100);
+            object_name = text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+            fill(255, 0, 109);
+            noFill();
+            stroke(255, 0, 109);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+            if(object[i].label == value)
+            {
+                video.stop();
+                objectdetection.detect(gotresults);
+                document.getElementById("detected").innerHTML = value + " Is found";
+                synth = window.speechSynthesis;
+                var utter_this = new SpeechSynthesisUtterance("Object mentioned found");
+                synth.speak(utter_this);
+            }
+            else
+            {
+                video.stop();
+                objectdetection.detect(gotresults);
+                document.getElementById("detected").innerHTML = value + " Is not found";
+                synth = window.speechSynthesis;
+                var utter_this = new SpeechSynthesisUtterance("Object mentioned not found");
+                synth.speak(utter_this);
+            }
+        }
+    }
+}
+function gotresults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        object = results;
+    }
 }
